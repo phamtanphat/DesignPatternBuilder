35722af88271156ecdf4f7fa01789b1229fd3490
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-20 20:21:58 +0700

    fast food

--- src/FastFood.java
+++ src/FastFood.java
@@ -1,4 +1,47 @@
 public class FastFood {
+    private Hamburger hamburger;
+    private Salad salad;
+    private Chicken chicken;
+    private Water water;
 
+    // alt + insert
 
+    public FastFood(Hamburger hamburger, Salad salad, Chicken chicken, Water water) {
+        this.hamburger = hamburger;
+        this.salad = salad;
+        this.chicken = chicken;
+        this.water = water;
+    }
+
+    public Hamburger getHamburger() {
+        return hamburger;
+    }
+
+    public void setHamburger(Hamburger hamburger) {
+        this.hamburger = hamburger;
+    }
+
+    public Salad getSalad() {
+        return salad;
+    }
+
+    public void setSalad(Salad salad) {
+        this.salad = salad;
+    }
+
+    public Chicken getChicken() {
+        return chicken;
+    }
+
+    public void setChicken(Chicken chicken) {
+        this.chicken = chicken;
+    }
+
+    public Water getWater() {
+        return water;
+    }
+
+    public void setWater(Water water) {
+        this.water = water;
+    }
 }
